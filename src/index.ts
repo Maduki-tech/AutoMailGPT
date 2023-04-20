4a6d09e78b3dead73d8f516f5d5d467834cd8c38
Maduki-tech <maduki.business@gmail.com>
2023-04-20 22:20:31 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,72 @@
+const fs = require("fs").promises;
+import path from "path";
+import process from "process";
+import { authenticate } from "@google-cloud/local-auth";
+import { GoogleApis, google } from "googleapis";
+
+type OAuth2Client = typeof GoogleApis.prototype.auth.OAuth2.prototype;
+type Auth = typeof GoogleApis.prototype.auth.OAuth2;
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), "token.json");
+const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
+
+async function loadSavedCredentialsIfExist(): Promise<OAuth2Client | null> {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials) as OAuth2Client;
+  } catch (err) {
+    return null;
+  }
+}
+
+async function saveCredentials(client: OAuth2Client): Promise<void> {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: "authorized_user",
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+async function listLabels(auth: any) {
+  const gmail = google.gmail({ version: "v1", auth });
+  const res = await gmail.users.labels.list({
+    userId: "me",
+  });
+  const labels = res.data.labels;
+  if (!labels || labels.length === 0) {
+    console.log("No labels found.");
+    return;
+  }
+  console.log("Labels:");
+  labels.forEach((label) => {
+    console.log(`- ${label.name}`);
+  });
+}
+
+authorize().then(listLabels).catch(console.error);
